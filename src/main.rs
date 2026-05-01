2d370a7a417a955c350296334d05df6af328c015
Maskrenhas2022 <maskrenhas02@gmail.com>
2026-05-01 12:48:26 -0400

    comeback today for my code rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod utils;
 
+
+
+use std::process::exit;
+
 // Importando as funções do seu módulo terminal
 use utils::terminal::{esperar_enter, exibir_menu};
 
@@ -19,6 +23,17 @@ fn main() {
     // Note que agora passei 5 itens para combinar com o que você queria
     
  let _selecionado = exibir_menu("Principal", &itens, true);
+    
+    match _selecionado {
+        1 => println!("1"),
+        2 => println!("2"),
+        3 => println!("3"),
+        4 => println!("4"),
+        5 => println!("5"),
+
+        _ => exit(0),
+    }
+    
     esperar_enter();
 }
 
